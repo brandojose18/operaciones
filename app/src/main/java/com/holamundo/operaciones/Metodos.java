ffd0d24f9aab3d33112ea6c5144d3227065f69aa
Brando <brandojose18@gmail.com>
2018-03-05 20:25:12 -0500

    Operaciones sin testing

--- app/src/main/java/com/holamundo/operaciones/Metodos.java
+++ app/src/main/java/com/holamundo/operaciones/Metodos.java
@@ -6,7 +6,13 @@ package com.holamundo.operaciones;
 
 public class Metodos {
 
-    public static double sumar(double n1, double n2){
-        return n1+n2;
-    }
+    public static double sumar(double n1, double n2){return n1+n2; }
+
+    public static double restar(double n1, double n2){return n1-n2; }
+
+    public static double multiplicar(double n1, double n2){return n1*n2; }
+
+    public static double dividir(double n1, double n2){return n1/n2; }
+
+
 }
