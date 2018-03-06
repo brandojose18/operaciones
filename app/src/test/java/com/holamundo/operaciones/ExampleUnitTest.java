bd56e2e669c9ebc6f7b4a18f9494adf8d6c3f01b
Brando <brandojose18@gmail.com>
2018-03-05 21:31:22 -0500

    Operaciones con testing

--- app/src/test/java/com/holamundo/operaciones/ExampleUnitTest.java
+++ app/src/test/java/com/holamundo/operaciones/ExampleUnitTest.java
@@ -11,7 +11,35 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void sumar_dos_numeros_correctamente(){
+        double a=4, b=5;
+        double res = Metodos.sumar(a,b);
+        assertEquals(9,res,0);
+
     }
+
+    @Test
+    public void restar_dos_numeros_correctamente(){
+        double a=4, b=5;
+        double res = Metodos.restar(a,b);
+        assertEquals(9,res,0);
+
+    }
+
+    @Test
+    public void multiplicar_dos_numeros_correctamente(){
+        double a=4, b=5;
+        double res = Metodos.multiplicar(a,b);
+        assertEquals(9,res,0);
+
+    }
+    
+    @Test
+    public void dividir_dos_numeros_correctamente(){
+        double a=4, b=5;
+        double res = Metodos.dividir(a,b);
+        assertEquals(9,res,0);
+
+    }
+
 }
